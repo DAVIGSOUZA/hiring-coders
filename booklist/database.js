20159073147fced2f2bc6ed595909211e2903471
Davi Garcia <davigs@hotmail.com>
2021-07-08 11:38:00 -0300

    add database mock

---
+++ booklist/database.js
@@ -0,0 +1,84 @@
+const books = [
+  {
+    id: 1,
+    title: "Foo",
+    author: "Fulano",
+    category: "Ficção",
+    pages: 200,
+    recommended: false 
+  },
+  {
+    id: 2,
+    title: "Bar",
+    author: "Ciclano",
+    category: "Ficção",
+    pages: 200,
+    recommended: false 
+  },
+  {
+    id: 3,
+    title: "Bananinha",
+    author: "Beltrano",
+    category: "Culinária",
+    pages: 100,
+    recommended: false 
+  },
+  {
+    id: 4,
+    title: "Pilares da Terra VOL 1",
+    author: "Ken Follet",
+    category: "Ficção histórica",
+    pages: 400,
+    recommended: true 
+  },
+  {
+    id: 5,
+    title: "Senhor dos Anéis - Sociedade do Anel",
+    author: "JRR Tolkien",
+    category: "Fantasia",
+    pages: 400,
+    recommended: true 
+  },
+  {
+    id: 6,
+    title: "Pilares da Terra VOL 2",
+    author: "Ken Follet",
+    category: "Ficção histórica",
+    pages: 400,
+    recommended: true 
+  },
+  {
+    id: 7,
+    title: "Senhor dos Anéis - As Duas Torres",
+    author: "JRR Tolkien",
+    category: "Fantasia",
+    pages: 400,
+    recommended: true 
+  },
+  {
+    id: 8,
+    title: "Senhor dos Anéis - O Retorno do Rei",
+    author: "JRR Tolkien",
+    category: "Fantasia",
+    pages: 400,
+    recommended: true 
+  },
+  {
+    id: 9,
+    title: "O Hobbit",
+    author: "JRR Tolkien",
+    category: "Fantasia",
+    pages: 400,
+    recommended: true 
+  },
+  {
+    id: 10,
+    title: "Mundo sem Fim",
+    author: "Ken Follet",
+    category: "Ficção histórica",
+    pages: 400,
+    recommended: true 
+  }
+]
+
+module.export = books
